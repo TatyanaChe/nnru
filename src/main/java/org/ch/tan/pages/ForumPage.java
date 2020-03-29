7fd640f1eb1c7d261f883d2dfcd813a74d72d913
Tatyana <Tatyana@192.168.1.72>
2020-03-29 22:39:37 +0300

    pagination

--- src/main/java/org/ch/tan/pages/ForumPage.java
+++ src/main/java/org/ch/tan/pages/ForumPage.java
@@ -8,17 +8,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class LastPage extends PageObject {
+public class ForumPage extends PageObject {
 
+	private int pageNumber;
+	private static final String URL_TEMPLATE = "https://www.nn.ru/community/my_baby/detskiy-transport/?Part=%s";
+	
 	@FindBy(xpath = "*//*[@class='heading-cont']/a[1]")
 	public List<WebElement> listAnchors;
 
-	public LastPage(WebDriver driver) {
+	public ForumPage(WebDriver driver, int pageNumber) {
 		super(driver);
+		this.pageNumber = pageNumber;
 	}
 
 	public void open() {
-		driver.get("https://www.nn.ru/community/my_baby/detskiy-transport/?Part=7");
+		driver.get("https://www.nn.ru/community/my_baby/detskiy-transport/?Part=1");
 	}
 //	*//a[contains(@href)]
 //	*//*[@class='heading-cont']//a[contains(@href)]
@@ -36,15 +40,17 @@ public class LastPage extends PageObject {
 		List<String> links = new ArrayList<String>();
 		for (WebElement listAnchor : listAnchors) {
 			// listAnchor.click();
+
 			String href = listAnchor.getAttribute("href");
 			links.add(href);
 			System.out.println(" + href: " + href);
 		}
 		List<String> found = new ArrayList<String>();
+		int it = 1;
 		for (String ln : links) {
 			try {
 				driver.get(ln);
-				System.out.println("ln: " + driver.getCurrentUrl());
+				System.out.println("ln " + it + ": " + driver.getCurrentUrl());
 				String bodyText = driver.findElement(By.tagName("body")).getText();
 //				System.out.println("bodyText: " + bodyText);
 				result = bodyText.contains("тема не актуальна");
@@ -56,6 +62,7 @@ public class LastPage extends PageObject {
 			} catch (Exception e) {
 				// TODO: handle exception
 			}
+			it = it + 1;
 		}
 		return found;
 
@@ -65,5 +72,28 @@ public class LastPage extends PageObject {
 		// TODO Auto-generated method stub
 
 	}
+
+	public boolean hasNextPage() {
+		boolean resultPageNotExist = false;
+		String bodyPageNotExist = driver.findElement(By.tagName("body")).getText();
+		if (bodyPageNotExist.contains("На этом форуме нет тем")) {
+			resultPageNotExist = true;
+		}
+		return resultPageNotExist;
+	}
+
+	public ForumPage nextPage() {
+		String nextUrl = String.format(URL_TEMPLATE, pageNumber++);
+		driver.get(nextUrl);
+		ForumPage page = new ForumPage(driver, pageNumber);
+		return page ;
+		
+	}
+
+	public int getPageNumber() {
+		return pageNumber;
+	}
+	
+	
 
 }
--- src/test/java/org/ch/tan/nnru/TestNnru.java
+++ src/test/java/org/ch/tan/nnru/TestNnru.java
@@ -2,12 +2,13 @@ package org.ch.tan.nnru;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.ch.tan.common.Browser;
 import org.ch.tan.common.DriverFactory;
 import org.ch.tan.common.Props;
-import org.ch.tan.pages.LastPage;
+import org.ch.tan.pages.ForumPage;
 import org.ch.tan.pages.MainPage;
 import org.junit.After;
 import org.junit.Before;
@@ -22,11 +23,13 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class TestNnru {
 
 	private WebDriver driver;
+//	MainPage mainPage = new MainPage(driver);
 
 	@Before
-	public void beforeTest() {
+	public void beforeTest() throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
 		driver = DriverFactory.get(Browser.chrome);
+		
 
 	}
 
@@ -68,19 +71,39 @@ public class TestNnru {
 		mainPage.clickButtonViceVersa();
 		mainPage.clickFilterByTitleSeek();
 		mainPage.clickButtonApply();
-//	Закрыть последнюю тему
+//	Список тем с закрывающими комментами
 //		Открыть последнюю тему в новой вкладке
-		LastPage lastPage = new LastPage(driver);
-		lastPage.open();
-		Thread.sleep(1000);
-		System.out.println("Последняя странца открыта");
-		System.out.println("listAnchors: " + lastPage.listAnchors.size());
-		System.out.println(lastPage.listAnchors);
-//		lastPage.clickLastAnchor();
-//		найти текст "тема не актуальна"
-		Thread.sleep(5000);
-		List<String> rrr = lastPage.foundLinks();
-		System.out.println("lastPage.foundLinks() " + rrr);
+//		ForumPage lastPage = new ForumPage(driver);
+//		lastPage.open();
+//		Thread.sleep(1000);
+//		System.out.println("listAnchors: " + lastPage.listAnchors.size());
+//		System.out.println(lastPage.listAnchors);
+////		найти текст "тема не актуальна"
+//		Thread.sleep(5000);
+//		List<String> foundLinks = lastPage.foundLinks();
+//		System.out.println("lastPage.foundLinks() " + foundLinks);
+	
+//	Найти линки по всем страницам
+		ForumPage forumPage = new ForumPage(driver, 1);
+//		forumPage.open();
+//		Thread.sleep(1000);
+//		forumPage.foundLinks();
+		List<String> allLinks = new ArrayList<String>();
+		while (!forumPage.hasNextPage()) {
+			System.out.println("getPageNumber: " + forumPage.getPageNumber());
+			forumPage = forumPage.nextPage();
+			List<String> list = forumPage.foundLinks();
+			allLinks.addAll(list);
+		}
+		
+		int i =1;
+		System.out.println("  allLinks.size: " + allLinks.size());
+		for (String string : allLinks) {
+			System.out.println("   " + i++ + string);
+		}
+		System.out.println("===== stream ===========" );
+		allLinks.stream().forEach(ln -> System.out.println(ln));
+	
 	}
 
 }
